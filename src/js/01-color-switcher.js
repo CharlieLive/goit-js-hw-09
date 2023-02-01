89af7baa2aae9896fb6bbc174d7084c09421080f
CharlieLive <karol.kozaczuk@gmail.com>
2023-02-01 14:19:13 +0100

    update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const background = document.querySelector('body');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let intervalId;
+
+startBtn.addEventListener('click', () => {
+  startBtn.disabled = true;
+  intervalId = setInterval(() => {
+    background.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  startBtn.disabled = false;
+  clearInterval(intervalId);
+});
+
 
